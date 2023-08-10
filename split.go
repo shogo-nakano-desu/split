b66939bad9bce45b9cac4831f11bdf02e1f8f764
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2023-08-10 22:57:54 +0900

    test: impl test for utils

--- split.go
+++ split.go
@@ -84,3 +84,31 @@ func SplitByBytes(file *os.File, byteSize int, baseFileName string, suffixLen in
 		fileIdx++
 	}
 }
+
+// writeToFile is a function that writes the given content to the file.
+func writeToFile(content string, baseFileName string, suffix string) {
+	if baseFileName == "" {
+		baseFileName = "x"
+	}
+	newFileName := fmt.Sprintf("%s%s", baseFileName, suffix)
+	outFile, err := os.Create(newFileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		err := outFile.Close()
+		if err != nil {
+			fmt.Printf("Error closing the file: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	_, err = outFile.WriteString(content)
+	if err != nil {
+		fmt.Printf("Error writing to the file: %v\n", err)
+		os.Exit(1)
+	}
+
+}
--- utils.go
+++ utils.go
@@ -13,15 +13,19 @@ func NormalizeArgs(args []string) []string {
 	for i := 0; i < len(args); i++ {
 		if strings.HasPrefix(args[i], "-l") && len(args[i]) > 2 {
 			args = append(args[:i], append([]string{"-l", args[i][2:]}, args[i+1:]...)...)
-			break
+			// break
 		}
 		if strings.HasPrefix(args[i], "-n") && len(args[i]) > 2 {
 			args = append(args[:i], append([]string{"-n", args[i][2:]}, args[i+1:]...)...)
-			break
+			// break
 		}
 		if strings.HasPrefix(args[i], "-b") && len(args[i]) > 2 {
 			args = append(args[:i], append([]string{"-b", args[i][2:]}, args[i+1:]...)...)
-			break
+			// break
+		}
+		if strings.HasPrefix(args[i], "-a") && len(args[i]) > 2 {
+			args = append(args[:i], append([]string{"-a", args[i][2:]}, args[i+1:]...)...)
+			// break
 		}
 	}
 	return args
@@ -39,7 +43,7 @@ func GenerateStrings(length int, prefix string, counter int) []string {
 		return []string{prefix}
 	}
 	if length > 5 {
-		fmt.Println("Error: suffix length must be less than 5")
+		fmt.Println("Error: suffix length must be less than or equal to 5")
 		os.Exit(1)
 	}
 
@@ -62,24 +66,32 @@ type Args struct {
 
 // IllegalArgsChecker is a function that checks if the arguments passed to the program are valid.
 func IllegalArgsChecker(params Args) {
-	lineSet := false
-	fileSet := false
-	byteSet := false
+	lineSetCount := 0
+	fileSetCount := 0
+	byteSetCount := 0
 
 	lineCount, fileCount, byteSize, args := params.LineCount, params.FileCount, params.ByteSize, params.Args
 
+	fmt.Println(args)
 	for _, arg := range args {
 		switch arg {
 		case "-l":
-			lineSet = true
+			lineSetCount++
 		case "-n":
-			fileSet = true
+			fileSetCount++
 		case "-b":
-			byteSet = true
+			byteSetCount++
+		case "-a":
+			continue
+		default:
+			if strings.HasPrefix(arg, "-") {
+				fmt.Println("Unknown option:", arg)
+				os.Exit(1)
+			}
 		}
 	}
 
-	if (lineSet && fileSet) || (lineSet && byteSet) || (fileSet && byteSet) || (lineSet && fileSet && byteSet) {
+	if lineSetCount+fileSetCount+byteSetCount > 1 {
 		fmt.Println(
 			`usage: split [-l line_count] [-a suffix_length] [file [prefix]]
 			split -b byte_count[K|k|M|m|G|g] [-a suffix_length] [file [prefix]]
@@ -89,17 +101,17 @@ func IllegalArgsChecker(params Args) {
 		os.Exit(1)
 	}
 
-	if lineCount <= 0 && lineSet {
+	if lineCount <= 0 && lineSetCount == 1 {
 		fmt.Printf("split: %d: illegal line count\n", lineCount)
 		os.Exit(1)
 	}
 
-	if fileCount <= 0 && fileSet {
+	if fileCount <= 0 && fileSetCount == 1 {
 		fmt.Printf("split: %d: illegal file count\n", fileCount)
 		os.Exit(1)
 	}
 
-	if byteSize <= 0 && byteSet {
+	if byteSize <= 0 && byteSetCount == 1 {
 		fmt.Printf("split: %d: illegal byte size\n", byteSize)
 		os.Exit(1)
 	}
